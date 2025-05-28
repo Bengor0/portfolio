825e43e3e3b0226b9894c7a4cb2620d7338c5986
Viktor Sulla <sulla.viktor@gmail.com>
2025-05-28 12:01:16 +0200

    finished game 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useActionState } from "react";
 import "./App.css";
 
 const API_URL = "https://cheaderthecoder.github.io/5-Letter-words/words.json";
 const WORD_LENGTH = 5;
+const NUM_OF_GUESSES = 6;
+const BASE_COLORS = ["black", "black", "black", "black", "black"];
 
 export default function App() {
   const [solution, setSolution] = useState("");
-  const [guesses, setGuesses] = useState(new Array(6).fill(""));
+  const [guesses, setGuesses] = useState(new Array(6).fill(["", BASE_COLORS]));
   const refIndex = useRef(0);
-  const refGuess = useRef("");
+  const refGuess = useRef(["", BASE_COLORS]);
   const wordBank = useRef(new Set());
   const isGameover = useRef(false);
   const [guessMessage, setGuessMessage] = useState(null);
 
-  const updateGuesses = () => {
-    const shallowGuesses = [...guesses];
-    shallowGuesses[refIndex.current] = refGuess.current;
-    setGuesses(shallowGuesses);
-  };
-
-  const checkGuess = () => {
-    if (wordBank.current.has(refGuess.current)) {
-      for (let i = 0; i < WORD_LENGTH; i++){
-
-      }
+  const handleGameOver = () => {
+    if (!isGameover.current) {
+      setTimeout(() => {
+        isGameover.current = true;
+      }, 4000);
     } else {
-      setGuessMessage(<p className="not-a-word">Not a word!</p>);
+      isGameover.current = false;
     }
   };
 
@@ -46,25 +42,70 @@ export default function App() {
     fetchWords();
   }, []);
 
+  ////////////////////////////////////////////////////////////////////////////////////////
+
+  const updateGuesses = (colors) => {
+    const shallowGuess = [refGuess.current[0], colors];
+    refGuess.current = shallowGuess;
+    const shallowGuesses = [...guesses];
+    shallowGuesses[refIndex.current] = refGuess.current;
+    setGuesses(shallowGuesses);
+  };
+
+  const checkGuess = () => {
+    if (wordBank.current.has(refGuess.current[0])) {
+      let matchCount = 0;
+      const updateColors = [];
+      for (let i = 0; i < WORD_LENGTH; i++) {
+        if (refGuess.current[0][i] === solution[i]) {
+          updateColors.push("green");
+          matchCount++;
+        } else if (solution.includes(refGuess.current[0][i])) {
+          updateColors.push("yellow");
+        } else {
+          updateColors.push("grey");
+        }
+      }
+      updateGuesses(updateColors);
+      if (matchCount === WORD_LENGTH) {
+        setGuessMessage(<p className="correct">Correct!</p>);
+        return true;
+      } else {
+        setGuessMessage(<p className="try-again">Try again!</p>);
+        return false;
+      }
+    } else {
+      setGuessMessage(<p className="not-a-word">Not a word!</p>);
+      return false;
+    }
+  };
+
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.key === "Backspace" && refGuess.current.length > 0) {
-        refGuess.current = refGuess.current.slice(0, -1);
-        updateGuesses();
+      if (event.key === "Backspace" && refGuess.current[0].length > 0) {
+        refGuess.current[0] = refGuess.current[0].slice(0, -1);
+        updateGuesses(BASE_COLORS);
         return;
       }
-      if (event.key === "Enter" && refGuess.current.length === 5) {
-        refGuess.current = "";
-        refIndex.current++;
-        return;
+      if (event.key === "Enter" && refGuess.current[0].length === WORD_LENGTH) {
+        if (checkGuess() || refIndex.current === NUM_OF_GUESSES - 1) {
+          handleGameOver();
+          return;
+        } else {
+          const shallowGuess = ["", BASE_COLORS];
+          refGuess.current = shallowGuess;
+          refIndex.current++;
+          return;
+        }
       }
       if (
         /[a-zA-Z]/.test(event.key) &&
         event.key.length === 1 &&
-        refGuess.current.length < 5
+        refGuess.current[0].length < WORD_LENGTH
       ) {
-        refGuess.current += event.key.toUpperCase();
-        updateGuesses();
+        refGuess.current[0] += event.key.toUpperCase();
+        updateGuesses(BASE_COLORS);
+        return;
       }
     };
 
@@ -76,16 +117,15 @@ export default function App() {
   return (
     <>
       {guessMessage}
-      {!isGameover.current && (
-        <div>
-          <div>{solution}</div>
-          <div className="board">
-            {guesses.map((guess, index) => {
-              return <Row guess={guess} key={index} />;
-            })}
-          </div>
+      <div>
+        <div>{solution}</div>
+        <div className="board">
+          {guesses.map((guess, index) => {
+            return <Row guess={guess} key={index} />;
+          })}
         </div>
-      )}
+      </div>
+      {isGameover.current && <button onClick={handleGameOver}>Reset</button>}
     </>
   );
 }
@@ -94,9 +134,10 @@ function Row({ guess }) {
   const tiles = [];
 
   for (let i = 0; i < WORD_LENGTH; i++) {
-    const char = guess[i];
+    const char = guess[0][i];
+    const className = `tile ${guess[1][i]}`;
     tiles.push(
-      <div key={i} className="tile">
+      <div key={i} className={className}>
         {char}
       </div>
     );
---
+++ src/Try.jsx
@@ -0,0 +1,144 @@
+import { useRef, useState, useEffect } from "react";
+
+const API_URL = "https://cheaderthecoder.github.io/5-Letter-words/words.json";
+const WORD_LENGTH = 5;
+const NUM_OF_GUESSES = 6;
+const BASE_COLORS = ["black", "black", "black", "black", "black"];
+
+export default function Try() {
+  const solution = useRef("");
+  const guess = useRef(["", BASE_COLORS]);
+  const [guesses, setGuesses] = useState(
+    new Array(NUM_OF_GUESSES).fill(["", BASE_COLORS])
+  );
+  const rowIndex = useRef(0);
+  const isGameOver = useRef(false);
+  const restart = useRef(false);
+  const wordSet = useRef(new Set());
+  const [message, setMessage] = useState(<p className="message"></p>);
+
+  const restartGame = () => {
+    guess.current = ["", BASE_COLORS];
+    setGuesses(new Array(NUM_OF_GUESSES).fill(["", BASE_COLORS]));
+    rowIndex.current = 0;
+    isGameOver.current = false;
+    wordSet.current = new Set();
+    setMessage(<p className="message"></p>);
+    restart.current = !restart.current;
+  };
+
+  useEffect(() => {
+    const fetchWords = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        const wordArray = [...data.words];
+        wordArray.forEach((word) => wordSet.current.add(word.toUpperCase()));
+        const randomWord =
+          wordArray[Math.floor(Math.random() * (wordArray.length - 1))];
+        solution.current = randomWord.toUpperCase();
+        console.log(solution.current);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchWords();
+  }, [restart.current]);
+
+  const updateGuesses = () => {
+    const shallowGuesses = [...guesses];
+    shallowGuesses[rowIndex.current] = guess.current;
+    setGuesses(shallowGuesses);
+  };
+
+  const checkGuess = () => {
+    const colors = [];
+    let correctCount = 0;
+
+    for (let i = 0; i < WORD_LENGTH; i++) {
+      if (guess.current[0][i] === solution.current[i]) {
+        colors.push("green");
+        correctCount++;
+      } else if (solution.current.includes(guess.current[0][i])) {
+        colors.push("orange");
+      } else colors.push("grey");
+    }
+
+    if (correctCount === WORD_LENGTH) {
+      setMessage(<Message message={"Correct!"} />);
+      isGameOver.current = true;
+    } else setMessage(<Message message={"Try again."} />);
+
+    guess.current = [guess.current[0], colors];
+    updateGuesses();
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (!isGameOver.current) {
+        if (event.key === "Enter" && guess.current[0].length === WORD_LENGTH) {
+          if (wordSet.current.has(guess.current[0])) {
+            checkGuess();
+            rowIndex.current = rowIndex.current + 1;
+            guess.current = ["", BASE_COLORS];
+            if (rowIndex.current >= NUM_OF_GUESSES) {
+              isGameOver.current = true;
+              return;
+            }
+          } else {
+            setMessage(<Message message={"Not in the word bank!"} />);
+            return;
+          }
+        } else if (rowIndex.current < NUM_OF_GUESSES) {
+          if (event.key === "Backspace" && guess.current[0].length > 0) {
+            guess.current[0] = guess.current[0].slice(0, -1);
+            updateGuesses(guess.current);
+          } else if (
+            /[a-zA-Z]/.test(event.key) &&
+            event.key.length === 1 &&
+            guess.current[0].length < WORD_LENGTH
+          ) {
+            guess.current[0] = guess.current[0] + event.key.toUpperCase();
+            updateGuesses(guess.current);
+          } else return;
+        }
+      } else return;
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [guesses]);
+
+  return (
+    <>
+      {message}
+      {guesses.map((guess, i) => {
+        return <Row guess={guess} key={i} />;
+      })}
+      {isGameOver.current === true && <button onClick={restartGame}>Play again</button>}
+    </>
+  );
+}
+
+function Row({ guess }) {
+  const tiles = [];
+
+  for (let i = 0; i < WORD_LENGTH; i++) {
+    let char = guess[0][i];
+    let className = `tile ${guess[1][i]}`;
+
+    tiles.push(
+      <div className={className} key={i}>
+        {char}
+      </div>
+    );
+  }
+
+  return <div className="row">{tiles}</div>;
+}
+
+function Message({ message }) {
+  return <p className="message">{message}</p>;
+}
